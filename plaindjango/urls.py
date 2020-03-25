49f8d59c3c3cdefbef4cf58dca0b84c65cdbff92
ramandahiyardx8 <62638934+ramandahiyardx8@users.noreply.github.com>
2020-03-25 15:23:06 +0530

    Add files via upload

--- plaindjango/urls.py
+++ plaindjango/urls.py
@@ -1,4 +1,4 @@
-"""plaindjango URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index,name='index'),
+    path('about', views.about,name='about'),
+    path('remove', views.remove,name="remove")
+
 ]
